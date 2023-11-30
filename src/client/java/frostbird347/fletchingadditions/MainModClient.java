3254db93fee3f719acc87de9c3955ca1b9d5f9c3
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2023-11-30 12:02:28 +0800

    Import template
    from https://fabricmc.net/develop/template/

---
+++ src/client/java/frostbird347/fletchingadditions/MainModClient.java
@@ -0,0 +1,10 @@
+package frostbird347.fletchingadditions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
